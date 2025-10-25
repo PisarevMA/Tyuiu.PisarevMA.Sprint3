9ca847d26e10c78560db8b1365ef398eefc18c07
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-10-25 14:53:46 +0500

    Add project files.

---
+++ Tyuiu.PisarevMA.Sprint3.Task0.V25/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.PisarevMA.Sprint3.Task0.V25.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт 3 | Выполнил: Писарев М. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла for                                                *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #25                                                             *");
+Console.WriteLine("* Выполнил: Писарев Максим Алексеевич | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+Console.WriteLine("* по формуле                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int value = 5;
+int startValue = 1;
+int stopValue = 14;
+
+Console.WriteLine("Переменная X = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue)); 
+
+Console.ReadKey();
